95b1d5f6ce176e7b187778d195f347533402377f
Mahmood-47 <nyendemahmood386@gmail.com>
2018-06-09 15:18:27 +0300

    my update

---
+++ er.R
@@ -0,0 +1,19 @@
+runif(10)
+
+set.seed(10)
+runif(10)
+
+
+set.seed(10)
+runif(10)
+
+options(digits = 4)
+runif(10,2,8)
+
+sample(1:6,5)
+
+sample(1:6,5,replace=TRUE)  
+
+sort(sample(2:10,10,replace=TRUE))
+
+sort(sample(1:5,5,replace=TRUE))
